6f68ddf45de48cca45e8468eae65a0741e935361
Marcin Kaszynski <>
2018-10-15 18:47:50 +0200

    Bump version number.

--- jwt_devices/__init__.py
+++ jwt_devices/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = "Django Rest Framework JWT Devices"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "Michal Proszek"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017-2018 Arabella"
