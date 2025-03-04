a1250b54c44d18242a8df0212e151eb38850b682
arencro <109680198+arencro@users.noreply.github.com>
2025-03-04 13:27:34 -0600

    Arenc/update setup for build (#15)
    * Fix setup.py for deprecation changes
    
    * Missing long desc type and status update
    
    * Further cleanup

--- jwt_devices/__init__.py
+++ jwt_devices/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 __title__ = "Django Rest Framework JWT Devices"
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 __author__ = "Michal Proszek"
 __license__ = "MIT"
-__copyright__ = "Copyright 2017-2018 Arabella"
+__copyright__ = "Copyright 2017-2018 Arabella 2018 - 2025 Roman Health Ventures"
 
 # Version synonym
 VERSION = __version__
